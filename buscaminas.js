9d4dba3cc9b7d7b637e2235b38fc3b71f3c02fc7
ErandyR <rerandy15@gmail.com>
2017-04-04 20:43:06 +0000

    funcionan las celdas

--- buscaminas.js
+++ buscaminas.js
@@ -1,11 +1,30 @@
-var botonNumero = document.getElementById("numero");
-//var botonBomba = document.getElementsByClassName("bomba");
-//var botonCambiarColor = document.getElementsByClassName("vacio");
+var botonNumero = document.getElementsByClassName("numero");
+var botonBomba = document.getElementsByClassName("bomba");
+var botonCambiarColor = document.getElementsByClassName("vacio");
 
-botonNumero.addEventListener("click", mostrarNumero);
+for(var i=0; i<botonNumero.length; i++){
+  botonNumero[i].addEventListener("click", mostrarNumero);
+  console.log(botonNumero);
+}
+for(var i=0; i<botonBomba.length; i++){
+  botonBomba[i].addEventListener("click", mostrarBomba);
+}
+for(var i=0; i<botonCambiarColor.length; i++){
+  botonCambiarColor[i].addEventListener("click", cambiarColor);
+}
 
-
-console.log(botonNumero);
 function mostrarNumero(){
-botonNumero.innerHTML = "1";
+this.innerText = "1";
+
+}
+function mostrarBomba(){
+  var miImagen = document.createElement("img");
+  miImagen.setAttribute("src", "assets/bomba.ico");
+  miImagen.setAttribute("width","25px");
+  miImagen.setAttribute("height","25px");
+  this.appendChild(miImagen);
+}
+function cambiarColor(){
+
+  this.style.backgroundColor = "#C3F3D7"
 }
